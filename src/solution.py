df333f559d9df008b98bab107a48937def5bf0ea
YoshiroSenpai <123640832+YoshiroTheDestroyer@users.noreply.github.com>
2023-09-10 23:50:56 +0800

    updated the code

--- src/solution.py
+++ src/solution.py
@@ -1,4 +1,6 @@
 import math as M  
 Radius = float (input ("Please enter a radius: "))  
 area_of_circle = M.pi* Radius * Radius
-print (" The area for the circle is: ", area_of_circle)  
+decimal_places = int(input("Please enter the number of decimal places: "))
+rounded_area = round(area_of_circle, decimal_places)
+print (f"The area for the circle is: {rounded_area:.{decimal_places}f}")
