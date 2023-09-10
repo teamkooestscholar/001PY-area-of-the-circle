ccfeeee7ae5027251290e361c1d390087363260d
ysabelandreii <123624285+ysabelandreii@users.noreply.github.com>
2023-09-10 15:47:20 +0800

    Update solution.py
    Submission of solution.py for 001PY - Calculate the area of the circle

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,31 @@
-# Write a function that calculates the area of a circle by a given radius.
-def calculate_circle_area(_):
-    pass
+import math
+
+def calculate_circle_area(radius, decimal_places=None):
+    try:
+        assert radius >= 0, "Radius must be non-negative."
+
+        area = math.pi * (radius ** 2)
+
+        if decimal_places is not None:
+            area = round(area, decimal_places)
+
+        return area
+    except AssertionError as e:
+        return str(e)
+
+def main():
+    try:
+        radius = float(input("Enter the radius of the circle: "))
+        decimal_places = int(input("Enter the number of decimal places to round to: "))
+        
+        result = calculate_circle_area(radius, decimal_places)
+        
+        if isinstance(result, str):
+            print(f"Error: {result}")
+        else:
+            print(f"The area of the circle is: {result}")
+    except ValueError:
+        print("Invalid input. Please enter a valid numeric radius.")
+
+if __name__ == "__main__":
+    main()
