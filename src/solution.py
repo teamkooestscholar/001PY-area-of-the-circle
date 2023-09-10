60f10ac827c6f8e2f7ddd87d6dfd18342bb0ff48
PlopN <70200493+TadWeird@users.noreply.github.com>
2023-09-10 21:55:55 +0800

    Code Implemented

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,22 @@
-# Write a function that calculates the area of a circle by a given radius.
-def calculate_circle_area(_):
-    pass
+#import math #If math.pi is implemented the value will not result with decimal places.
+try:
+    radius = float(input("Register The Radius Length: "))
+    
+    # [V] This implies that the radius should not be less than 0 or a negative number, but can be 0.
+    assert radius >= 0, "Radius must be non-negative and should not be less than 0"
+
+    Deci = int(input("Enter the number of decimal places for rounding: " ))
+    Pie = 3.141592653589793238462643
+
+    # [V] Formula to compute the Radius of the Circle
+    #math.pi could be utilized for this process however it does not result with decimal points.
+    #CircArea = round(Pie*radius*radius, Deci) #functions the same, just different layout.
+    CircArea = round(Pie*radius**2) #
+    
+    
+    # [V] Output
+    print("Result of Evaluated Radius of The Circle: ", CircArea) 
+
+# [V] Error Prompt If Value is Null or Invalid
+except ValueError: 
+    print("Seems Like You've registered an Invalid Variable. Try Again.")
