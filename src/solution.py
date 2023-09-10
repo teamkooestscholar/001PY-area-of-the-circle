7aac6fd65c0458005401528e877d0833a30a4eaf
lindraaa <chancruz739@gmail.com>
2023-09-10 14:59:06 +0800

    function created

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,10 @@
 # Write a function that calculates the area of a circle by a given radius.
-def calculate_circle_area(_):
-    pass
+def calculate_circle_area(radius):
+    assert radius > 0
+    total = 3.141559*(radius**2)
+    return total
+
+
+
+num = float(input("Enter the radius of a circle: "))
+print("The area is --> ", round(calculate_circle_area(num)))
