76b93bb3bf0abac57fba0698a4f2d654d436766b
SparrowNado <123578310+SparrowNado@users.noreply.github.com>
2023-09-10 21:19:27 +0800

    added solution

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,25 @@
-# Write a function that calculates the area of a circle by a given radius.
-def calculate_circle_area(_):
-    pass
+
+# Modify the function to handle negative radii gracefully and return an error message. (Hint: use the assert statement) [+5 extra credit points]
+#
+# Create a Python script that prompts the user to input a radius and then displays the calculated area. (Hint: use the input function) [+5 extra credit points]
+#
+# Enhance the function to round the calculated area to a specified number of decimal places. (Hint: use the round function) [+5 extra credit points
+#
+
+
+
+
+try:
+
+    r = float(input("Enter the Radius value"))
+
+
+    assert r >= 0, "Radius must be non-negative"
+
+    decimal_place = int(input("Enter the number of decimal places for rounding: " ))
+
+    area = round(3.14 * r * r, decimal_place)
+
+    print("The area of the circle is ", area)
+except ValueError:
+    print("Input is invalid Please enter a valid number next time ")
