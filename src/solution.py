50f700248105ebc54f57bfa57b629305a5a0e9b6
jlfx09 <142736901+jlfx09@users.noreply.github.com>
2023-09-10 22:58:35 +0800

    updated solution.py

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,16 @@
-# Write a function that calculates the area of a circle by a given radius.
-def calculate_circle_area(_):
-    pass
+import math
+
+def calculate_circle_area(radius, decimal_places=2):
+    if radius < 0:
+        return "Error: Radius cannot be negative"
+    else:
+        area = math.pi * (radius ** 2)
+        rounded_area = round(area, decimal_places)
+        return rounded_area
+
+# Example usage with assert statement:
+radius = float(input("Enter the radius of the circle: "))
+decimal_places = int(input("Enter the number of decimal places for rounding: "))
+
+result = calculate_circle_area(radius, decimal_places)
+print(f"The area of the circle with radius {radius} is approximately {result}")
