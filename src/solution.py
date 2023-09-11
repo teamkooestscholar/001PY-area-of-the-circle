c390e36224654129e1aa03753226089547b43383
Prgrmmr633 <airesmacalino09@gmail.com>
2023-09-11 08:38:45 +0800

    update

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,28 @@
-# Write a function that calculates the area of a circle by a given radius.
-def calculate_circle_area(_):
-    pass
+import math
+
+def calculate_circle_area(radius, decimal_places=None):
+    try:
+        assert radius >= 0, "Radius cannot be negative"
+        area = math.pi * (radius ** 2)
+        if decimal_places is not None:
+            area = round(area, decimal_places)
+        return area
+    except AssertionError as e:
+        return str(e)
+
+# Bonus Challenge 1
+print(calculate_circle_area(-5))
+print(calculate_circle_area(5))
+
+# Bonus Challenge 2:
+if __name__ == "__main__":
+    try:
+        radius = float(input("Enter the radius of the circle: "))
+        decimal_places = int(input("Enter the number of decimal places (or press Enter for no rounding): "))
+        result = calculate_circle_area(radius, decimal_places)
+        print(f"The area of the circle is: {result}")
+    except ValueError:
+        print("Invalid input. Please enter a valid number for the radius and decimal places.")
+
+# Bonus Challenge 3
+print(calculate_circle_area(5, 2))
