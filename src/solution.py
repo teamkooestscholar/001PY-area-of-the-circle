b3972e9829decf712225c30c6d2bf6d9ae27d97b
Tsukkiz <123831791+Tsukkiz@users.noreply.github.com>
2023-09-12 22:24:12 +0800

    PY1Kenneth

--- src/solution.py
+++ src/solution.py
@@ -1,3 +1,16 @@
-# Write a function that calculates the area of a circle by a given radius.
-def calculate_circle_area(_):
-    pass
+import math
+def calculate_circle_area():
+    radius = float(input("Enter Desired Radius: "))
+    
+    if radius < 0:
+        return "Error: Radius cannot be negative."
+    
+    area = math.pi * (radius ** 2)
+    rounded_area = round(area, 2)
+    return rounded_area
+
+area = calculate_circle_area()
+if isinstance(area, str):
+    print(area)
+else:
+    print(f"The area is {area}.")
