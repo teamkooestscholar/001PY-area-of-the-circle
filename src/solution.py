e316ac2b7adac06996d40dcaebe5eb040a4e6608
RaphaelSyrrusDNucum <144664548+RaphNucum@users.noreply.github.com>
2023-09-12 23:05:46 +0800

    raph

--- src/solution.py
+++ src/solution.py
@@ -11,3 +11,4 @@ if __name__ == "__main__":
     radius = float(input("Enter the radius of the circle: "))
     area = calculate_circle_area(radius)
     print(f"The area of the circle with radius {radius} is {area}")
+
